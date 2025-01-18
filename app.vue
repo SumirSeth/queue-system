a8c3bfa9814930c548797aa0f050744ceed93887
Sumir Seth <iamsumirseth@gmail.com>
2025-01-19 03:05:23 +0530

    add Tailwind CSS and custom components for task queue interface

--- app.vue
+++ app.vue
@@ -1,6 +1,91 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-black min-h-screen text-white font-[Geist]">
+    <Transition name="fade">
+      <div class="flex flex-col items-center justify-center h-screen" v-if="intro">
+        <h1 class="text-6xl font-bold">
+          <TextGenerateEffect words="Queue System" :duration=0.5 :delay=500 class=""/>
+        </h1>
+        <p class="text-xl">
+          <TextGenerateEffect words="A backend visualised by frontend project." :duration=0.1 :delay=1000 :filter=false class="" />
+        </p>
+      </div>
+
+      <div v-else class="flex flex-col items-center justify-center">
+        <p class="text-5xl font-thin">Queue System with custom Redis Implementation</p>
+      </div>
+
+    </Transition>
+
+
+    <div class="gap-2 grid grid-cols-2 items-center justify-center place-items-center h-screen">
+      <div>
+        <button @click="getAllTasks" class="bg-white p-2 text-black rounded-md">Get All Tasks</button>
+        <p>Queue Tasks:</p>
+        <p v-for="task in tasks" :key="task.key">
+          {{ task.key }} - {{ task.data }}
+          <br>
+        </p>
+      </div>
+      <div class="flex flex-col">
+        <button @click="addTask" class="bg-white p-2 text-black rounded-md">Add Task</button>
+        <input v-model="taskId" type="text" placeholder="Task ID" class="bg-gray-950">
+        <input v-model="taskData" type="text" placeholder="Task Data" class="bg-gray-950">
+      </div>
+    </div>
+
+
+
   </div>
 </template>
+
+
+<script setup lang="ts">
+
+const intro = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    intro.value = false
+  }, 3500)
+})
+
+const tasks = ref()
+
+const getAllTasks = async () => {
+  const response = await fetch('api/queue/fetch')
+  const data = await response.json()
+  tasks.value = data.tasks
+}
+
+const taskId = ref()
+const taskData = ref()
+
+const addTask = async () => {
+  const response = await fetch('api/queue/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      taskId: taskId.value,
+      taskData: taskData.value,
+    })
+  })
+}
+
+</script>
+
+
+
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
