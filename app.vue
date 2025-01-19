a1d7e31319d945a45a23546c55ee522214bbb04f
Sumir Seth <iamsumirseth@gmail.com>
2025-01-19 20:08:25 +0530

    refactor app.vue to enhance meta section, improve tooltip functionality, and add custom scrollbar styles

--- app.vue
+++ app.vue
@@ -1,19 +1,30 @@
 <template>
+
+  <!-- Meta section -->
   <Link rel="icon" href="/favicon.png" />
   <Title>Queue System</Title>
   <Meta name="description" content="Queue System with custom Redis Implementation" />
+  <!-- End of Meta section -->
 
+
+  <!-- Main div -->
   <div class="bg-black min-h-screen text-white font-[Geist]">
+
+    <!--FIXED BLOCKS:  Icons - Help and Close with Tooltip -->
     <Icon class="fixed top-3 right-3 hover:cursor-help size-8"  name="carbon:information-square-filled" @click="tooltip = !tooltip" v-if="!intro"/>
     <div v-if="tooltip" class="fixed flex flex-col h-screen w-screen items-center justify-center backdrop-blur-lg text-white rounded-md p-2 text-sm">
       <button>
         <Icon name="line-md:close-circle-filled" class="fixed top-3 right-6 size-9" @click="tooltip = !tooltip" />
       </button>
-      <p class="text-xl shadow-xl">This project is a visualisation of a queue system using Redis as the backend. The redis implementation is custom and does not use any external libraries.</p>
+      <p class="text-xl shadow-xl">This project is a visualisation of a queue system using Redis as the backend. The Redis implementation is custom and does not use any external libraries.</p>
       <p class="text-xl shadow-xl"></p>
     </div>
-    <Transition name="fade">
-      <div class="flex flex-col items-center justify-center h-screen" v-if="intro">
+    <!-- FIXED BLOCKS:  End of Icons - Help and Close with Tooltip -->
+
+
+    <!-- Intro section -->
+    <Transition name="fade" mode="out-in">
+      <div key="intro" class="flex flex-col items-center justify-center h-screen" v-if="intro">
         <h1 class="text-6xl font-bold">
           <TextGenerateEffect words="Queue System" :duration=0.5 :delay=500 class=""/>
         </h1>
@@ -21,29 +32,21 @@
           <TextGenerateEffect words="A backend visualised by frontend project." :duration=0.1 :delay=1000 :filter=false class="" />
         </p>
       </div>
-
-      <div v-else class="flex flex-col items-center justify-center">
+      
+      <div key="main" v-else class="flex flex-col items-center justify-center">
         <p class="text-5xl font-thin">Queue System with custom Redis Implementation</p>
       </div>
-
     </Transition>
+    <!-- End of Intro section -->
 
 
-    <div class="gap-2 grid grid-cols-2 items-center justify-center place-items-center h-screen">
-      <div>
-        <button @click="getAllTasks" class="bg-white p-2 text-black rounded-md">Get All Tasks</button>
-        <p>Queue Tasks:</p>
-        <p v-for="task in tasks" :key="task.key">
-          {{ task.key }} - {{ task.data }}
-          <br>
-        </p>
-      </div>
-      <div class="flex flex-col">
-        <button @click="addTask" class="bg-white p-2 text-black rounded-md">Add Task</button>
-        <input v-model="taskId" type="text" placeholder="Task ID" class="bg-gray-950">
-        <input v-model="taskData" type="text" placeholder="Task Data" class="bg-gray-950">
-      </div>
+    <!-- Main section -->
+    <div v-if="!intro">
+      <p>efwefwe</p>
+
     </div>
+
+
 
 
 
@@ -100,5 +103,29 @@ const addTask = async () => {
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  transition: opacity 0.5s ease;
+  opacity: 1;
+}
+
+.fade-move {
+  transition: transform 0.5s ease;
+}
+</style>
+
+<style>
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+::-webkit-scrollbar-track {
+  background: #000000;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #313131;
+  border-radius: 3px;
 }
 </style>
